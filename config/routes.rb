479525d4565d19231237c69f8c37ce0d9f35b420
ian crusan <iancrusan@hotmail.com>
2016-08-01 11:33:36 -0400

    good stopping place everything works

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'user/:id' => 'pages#profile'
   get 'pages/help' => 'pages#help'
   get 'pages/settings' => 'pages#settings'
+  get 'post/new'
+  get 'post/create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
